89f0659963f8da11c819eadf8ccaa5fe10dd3451
nolions <lv2410@gmail.com>
2019-10-26 23:01:03 +0800

    [fix] rleay example

--- relay_example/relay_example.ino
+++ relay_example/relay_example.ino
@@ -1,11 +1,12 @@
 /**
  * Relay control example
  * ==================================
- * 此範例使用的`高電平`的Relay，高電平繼電器在in 腳位在高電平狀態會讓Relay呈現通電的狀態，
- * 相反的在低電平狀態下則是呈現短路狀態。
+ * Relay與一般LED控制方式相同，皆是透過腳位電位高低來決定狀態。此範例使用的`高電平`的
+ * Relay，高電平繼電器在in 腳位在高電平狀態會讓Relay呈現通電的狀態，相反的在低電平狀
+ * 態下則是呈現短路狀態。
  * 
- * 如果今天使用`低電平`的Relay，`in`腳位在高電位會讓繼電器呈現短路狀態，低電位則會讓Relay
- * 呈現通電的狀態。
+ * 如果今天使用`低電平`的Relay，`in`腳位在高電位會讓繼電器呈現短路狀態，低電位則會讓
+ * Relay呈現通電的狀態。
  */
 const int Relay_IN_PIN = 4;
  
@@ -17,7 +18,7 @@ void setup() {
   pinMode(Relay_IN_PIN, OUTPUT);
 }
 
-void loop(bool isHight) {
+void loop() {
   if (Serial.available()) { // 檢查是Serial prot是否有資料
     // 利用從Serial接收到數據來控制Relay狀態
     char val = Serial.read();
@@ -33,7 +34,7 @@ void loop(bool isHight) {
 /**
  * 控制器繼電器狀態
  */
-void setRelay(double nowTemp) {
+void setRelay(bool isHight) {
   if (isHight) {
     digitalWrite(Relay_IN_PIN, HIGH);
   } else {

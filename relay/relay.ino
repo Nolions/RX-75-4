9c46a0a1fe81f761ba6d773ccfb7d79a414aa9db
nolions <lv2410@gmail.com>
2019-10-27 23:11:27 +0800

    [feature] 繼電器

---
+++ relay/relay.ino
@@ -0,0 +1,43 @@
+/**
+ * Relay control example
+ * ==================================
+ * Relay與一般LED控制方式相同，皆是透過腳位電位高低來決定狀態。此範例使用的`高電平`的
+ * Relay，高電平繼電器在in 腳位在高電平狀態會讓Relay呈現通電的狀態，相反的在低電平狀
+ * 態下則是呈現短路狀態。
+ * 
+ * 如果今天使用`低電平`的Relay，`in`腳位在高電位會讓繼電器呈現短路狀態，低電位則會讓
+ * Relay呈現通電的狀態。
+ */
+const int Relay_IN_PIN = 4;
+ 
+void setup() {
+  Serial.begin(9600);
+  Serial.println("=====控制Relay通/斷電狀態=====");
+
+  // 設定腳位模式
+  pinMode(Relay_IN_PIN, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available()) { // 檢查是Serial prot是否有資料
+    // 利用從Serial接收到數據來控制Relay狀態
+    char val = Serial.read();
+
+    if (val == 0) { // Serial收到1表示ON
+       setRelay(true); 
+    } else if(val == 1) {  // Serial收到1表示OFF
+      setRelay(false);
+    }
+  }
+}
+
+/**
+ * 控制器繼電器狀態
+ */
+void setRelay(bool isHight) {
+  if (isHight) {
+    digitalWrite(Relay_IN_PIN, HIGH);
+  } else {
+    digitalWrite(Relay_IN_PIN, LOW);
+  }
+}
